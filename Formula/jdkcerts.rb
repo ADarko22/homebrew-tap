f96730243666df6a1366105f2c94668d70d77bb7
ADarko22 <a.elessar22@gmail.com>
2026-01-11 20:30:46 +0100

    Fix JAR installation path in Homebrew formula for versioned JDKCertsTool

--- Formula/jdkcerts.rb
+++ Formula/jdkcerts.rb
@@ -1,30 +1,29 @@
 class Jdkcerts < Formula
   desc "Tool to manage JDK certificates"
   homepage "https://github.com/ADarko22/JDKCertsTool"
-  # These placeholders are filled automatically by the GitHub Action
   url "https://github.com/ADarko22/JDKCertsTool/releases/download/v2.0.0/JDKCertsTool-v2.0.0.jar"
   sha256 "6c2b4f22b1ac1c4c76785d255daafc160c8d0cbc63368dad927952beeec48241"
   license "MIT"
 
   depends_on "openjdk@21"
 
-  # It tells Homebrew to always skip the bottle search and install from the URL.
   def pour_bottle?
     false
   end
 
   def install
-    # 1. Move the JAR to libexec (internal library folder) and renames it to a clean name
-    libexec.install Dir["JDKCertsTool-*.jar"].first => "jdkcertstool.jar"
+    # 1. Move the jar to libexec
+    libexec.install "JDKCertsTool-v2.0.0.jar" => "jdkcertstool.jar"
 
-    # 2. Create the executable wrapper
-    # This creates a file at /usr/local/bin/jdkcerts (on Intel Mac)
-    # or /opt/homebrew/bin/jdkcerts (on Apple Silicon/Linux)
-    (bin/"jdkcerts").write_jar_script libexec/"jdkcertstool.jar", "jdkcerts", java_version: "21"
+    # 2. Create the script.
+      bin.write_jar_script(
+        libexec/"jdkcertstool.jar",
+        "jdkcerts",
+        java_version: "21"
+      )
   end
 
   test do
-    # This ensures 'jdkcerts --help' exits with 0
-    system "#{bin}/jdkcerts", "--help"
+    assert_match "JDK Management Tool", shell_output("#{bin}/jdkcerts info")
   end
 end
